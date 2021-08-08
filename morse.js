bbda7793f7a6d89f13781c69ead4a86ad23ed40d
Robert Dominguez <rtjdominguez@gmail.com>
2021-08-08 10:29:56 -0500

    init

---
+++ morse.js
@@ -0,0 +1,106 @@
+const morse = [
+  {
+    letter: `a`,
+    code: [0, 1],
+  },
+  {
+    letter: `b`,
+    code: [1, 0, 0, 0],
+  },
+  {
+    letter: `c`,
+    code: [1, 0, 1, 0],
+  },
+  {
+    letter: `d`,
+    code: [1, 0, 0],
+  },
+  {
+    letter: `e`,
+    code: [0],
+  },
+  {
+    letter: `f`,
+    code: [0, 0, 1, 0],
+  },
+  {
+    letter: `g`,
+    code: [1, 1, 0],
+  },
+  {
+    letter: `h`,
+    code: [0, 0, 0, 0],
+  },
+  {
+    letter: `i`,
+    code: [0, 0],
+  },
+  {
+    letter: `j`,
+    code: [0, 1, 1, 1],
+  },
+  {
+    letter: `k`,
+    code: [1, 0, 1],
+  },
+  {
+    letter: `l`,
+    code: [0, 1, 0, 0],
+  },
+  {
+    letter: `m`,
+    code: [1, 1],
+  },
+  {
+    letter: `n`,
+    code: [1, 0],
+  },
+  {
+    letter: `o`,
+    code: [1, 1, 1],
+  },
+  {
+    letter: `p`,
+    code: [0, 1, 1, 0],
+  },
+  {
+    letter: `q`,
+    code: [1, 1, 0, 1],
+  },
+  {
+    letter: `r`,
+    code: [0, 1, 0],
+  },
+  {
+    letter: `s`,
+    code: [0, 0, 0],
+  },
+  {
+    letter: `t`,
+    code: [1],
+  },
+  {
+    letter: `u`,
+    code: [0, 0, 1],
+  },
+  {
+    letter: `v`,
+    code: [0, 0, 0, 1],
+  },
+  {
+    letter: `w`,
+    code: [0, 1, 1],
+  },
+  {
+    letter: `x`,
+    code: [1, 0, 0, 1],
+  },
+  {
+    letter: `y`,
+    code: [1, 0, 1, 1],
+  },
+  {
+    letter: `z`,
+    code: [1, 1, 0, 0],
+  },
+];
